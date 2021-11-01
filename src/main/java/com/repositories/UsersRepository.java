d946d2c5257f421945d5c05533ee73400d1c7322
lukasz <lukasz@gmmail.com>
2021-11-01 23:00:02 +0100

    Change password feature added

--- src/main/java/com/repositories/UsersRepository.java
+++ src/main/java/com/repositories/UsersRepository.java
@@ -2,7 +2,15 @@ package com.repositories;
 
 import com.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.transaction.Transactional;
+import java.util.UUID;
 
 public interface UsersRepository extends JpaRepository<User, Integer> {
-
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query(value = "update public.users set password = :new_password where id = :user_id", nativeQuery = true)
+    @Transactional
+    void updatePassword(String new_password, UUID user_id);
 }
