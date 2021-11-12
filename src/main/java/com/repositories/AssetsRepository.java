88133ea9b82a920fd11d7cf00a1b98f41e3b85e3
lukasz <lukasz@gmmail.com>
2021-11-12 11:43:22 +0100

    Assets truncated to 2 digits

--- src/main/java/com/repositories/AssetsRepository.java
+++ src/main/java/com/repositories/AssetsRepository.java
@@ -2,11 +2,20 @@ package com.repositories;
 
 import com.models.Asset;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
+import javax.transaction.Transactional;
 import java.util.UUID;
 
 public interface AssetsRepository extends JpaRepository<Asset, Integer> {
 
     Asset findByUserID(UUID userID);
+
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query(value = "update public.assets set gold = :gold, silver = :silver, cash = :cash where id = :id", nativeQuery = true)
+    @Transactional
+    int updateAssets(UUID id, Double gold, Double silver, Double cash);
 
 }
